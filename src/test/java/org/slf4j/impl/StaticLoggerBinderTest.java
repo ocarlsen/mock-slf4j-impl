5181b5768f7f40287fe4b71326a542762a46cd21
Owen Carlsen <o@ocarlsen.com>
2021-07-21 17:05:23 -0700

    Using FieldUtils library to get private static value

--- src/test/java/org/slf4j/impl/StaticLoggerBinderTest.java
+++ src/test/java/org/slf4j/impl/StaticLoggerBinderTest.java
@@ -1,10 +1,9 @@
 package org.slf4j.impl;
 
 import com.ocarlsen.test.MockLoggerFactory;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Test;
 import org.slf4j.ILoggerFactory;
-
-import java.lang.reflect.Field;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
@@ -20,9 +19,10 @@ public class StaticLoggerBinderTest {
     }
 
     @Test
-    public void getSingleton() throws NoSuchFieldException, IllegalAccessException {
+    public void getSingleton() throws IllegalAccessException {
         final StaticLoggerBinder staticLoggerBinder = StaticLoggerBinder.getSingleton();
-        final StaticLoggerBinder singleton = getPrivateStaticValue();
+        final StaticLoggerBinder singleton =
+                (StaticLoggerBinder) FieldUtils.readDeclaredStaticField(StaticLoggerBinder.class, "SINGLETON", true);
         assertThat(staticLoggerBinder, is(sameInstance(singleton)));
     }
 
@@ -30,16 +30,6 @@ public class StaticLoggerBinderTest {
     public void getLoggerFactory() {
         final ILoggerFactory loggerFactory = StaticLoggerBinder.getSingleton().getLoggerFactory();
         assertThat(loggerFactory, is(instanceOf(MockLoggerFactory.class)));
-    }
-
-    // TODO: Factor out to utility
-    private StaticLoggerBinder getPrivateStaticValue() throws NoSuchFieldException, IllegalAccessException {
-        // Get field instance
-        final Field field = StaticLoggerBinder.class.getDeclaredField("SINGLETON");
-        field.setAccessible(true); // Suppress Java language access checking
-
-        // Get value
-        return (StaticLoggerBinder) field.get(null);
     }
 
 }
